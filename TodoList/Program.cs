b898284724941c99341745f52d4dff074e068b87
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-01 20:18:23 +0100

    some practice with vairable declarations, initializations and types

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,1 +1,18 @@
 ﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("[S]ee all TODOS");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
+Console.WriteLine(userInput);
+
+userInput = "ABC";
+Console.WriteLine(userInput);
+
+int number = 2;
+Console.WriteLine(number);
+number = 7;
+Console.WriteLine(number);
+
+Console.ReadKey();
