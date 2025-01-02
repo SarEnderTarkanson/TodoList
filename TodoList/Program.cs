c787c6f59c0bb0caafcc1ac7b833ab73ea3d3a0c
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-02 03:11:54 +0100

    practice for declaring variables and operands

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,18 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("[S]ee all TODOS");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
+﻿//Console.WriteLine("Hello, World!");
+//Console.WriteLine("[S]ee all TODOS");
+//Console.WriteLine("[A]dd a TODO");
+//Console.WriteLine("[R]emove a TODO");
+//Console.WriteLine("[E]xit");
 
-string userInput = "A";
-Console.WriteLine(userInput);
+//string userInput = "A";
+//Console.WriteLine(userInput);
 
-userInput = "ABC";
-Console.WriteLine(userInput);
+//userInput = "ABC";
+//Console.WriteLine(userInput);
 
-int number = 2;
-Console.WriteLine(number);
-number = 7;
-Console.WriteLine(number);
+//int number = 2;
+//Console.WriteLine(number);
+//number = 7;
+//Console.WriteLine(number);
+
+int a = 10;
+
+int b = 11;
+
+++a;
+
+--b;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
+
+Console.WriteLine("John" + " " + "Smith");
+
+string word = "abc";
+Console.WriteLine(word + a);
 
 Console.ReadKey();
