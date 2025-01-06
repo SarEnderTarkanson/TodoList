1ede0d3fe6e051418e2d94a20726f7713cc2c94d
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-06 02:18:47 +0100

    added a simple user input

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,39 +1,12 @@
-﻿//Console.WriteLine("Hello, World!");
-//Console.WriteLine("[S]ee all TODOS");
-//Console.WriteLine("[A]dd a TODO");
-//Console.WriteLine("[R]emove a TODO");
-//Console.WriteLine("[E]xit");
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOS");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
 
-//string userInput = "A";
-//Console.WriteLine(userInput);
+string userChoice = Console.ReadLine();
 
-//userInput = "ABC";
-//Console.WriteLine(userInput);
-
-//int number = 2;
-//Console.WriteLine(number);
-//number = 7;
-//Console.WriteLine(number);
-
-int a = 10;
-
-int b = 11;
-
-++a;
-
---b;
-
-Console.WriteLine(a);
-Console.WriteLine(b);
-
-Console.WriteLine(a + b);
-Console.WriteLine(a - b);
-Console.WriteLine(a * b);
-Console.WriteLine(a / b);
-
-Console.WriteLine("John" + " " + "Smith");
-
-string word = "abc";
-Console.WriteLine(word + a);
+Console.WriteLine("user input: " + userChoice);
 
 Console.ReadKey();
