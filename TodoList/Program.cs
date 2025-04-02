d262c6277e9213d56919d629d961a1c049ecaa0c
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-02 04:18:05 +0200

    Restarted the Udemy Course from the beginning

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,12 +1,2 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("What do you want to do?");
-Console.WriteLine("[S]ee all TODOS");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
-
-string userChoice = Console.ReadLine();
-
-Console.WriteLine("user input: " + userChoice);
-
+﻿Console.WriteLine("Hello");
 Console.ReadKey();
